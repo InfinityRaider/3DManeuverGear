27a4ce6a619b4f26a16edb9903956116c74b50a7
InfinityRaider <theoneandonlyraider@gmail.com>
2021-02-19 10:00:37 +0000

    Version bump

--- src/main/java/com/infinityraider/maneuvergear/reference/Reference.java
+++ src/main/java/com/infinityraider/maneuvergear/reference/Reference.java
@@ -7,9 +7,9 @@ public final class Reference {
 
     public static final String VER_MAJOR = /*^${mod.version_major}^*/ "2";
     public static final String VER_MINOR = /*^${mod.version_minor}^*/ "0";
-    public static final String VER_PATCH = /*^${mod.version_patch}^*/ "0";
-    public static final String MOD_VERSION = /*^${mod.version}^*/ "2.0.0";
-    public static final String VERSION = /*^${mod.version_minecraft}-${mod.version}^*/ "1.16.4-2.0.0";
+    public static final String VER_PATCH = /*^${mod.version_patch}^*/ "2";
+    public static final String MOD_VERSION = /*^${mod.version}^*/ VER_MAJOR + "." + VER_MINOR + "." + VER_PATCH;
+    public static final String VERSION = /*^${mod.version_minecraft}-${mod.version}^*/ "1.16.4-" + MOD_VERSION;
 
     public static final String UPDATE_URL = /*^${mod.update_url}^*/ "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
 }
